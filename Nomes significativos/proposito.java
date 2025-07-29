0e58cc11b643eacda353c506f7504b93d9ffa0ec
Nalu <pordosol699@gmail.com>
2025-07-29 11:29:08 -0300

    atualizando

--- Nomes significativos/proposito.java
+++ Nomes significativos/proposito.java
@@ -66,3 +66,19 @@ else
     l = 01;
 
 //confuso e desnecessário
+
+/*Usar palavras pronunciaveis é melhor do que palavras sem sentido e não comentaveis 
+olhe a diferença disso*/
+class DtaRcrd102{
+    private Date genymdhms;
+    private Date modymdhms;
+    private final String pszqint = "102";
+    /* . . . */
+};
+//e isso
+class Customer{
+    private Date generationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId = "102";
+    /* . . . */
+};

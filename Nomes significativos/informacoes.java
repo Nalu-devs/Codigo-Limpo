4cdba5120665179504105c8b5c19f518b713b645
Nalu <pordosol699@gmail.com>
2025-07-29 10:58:06 -0300

    aqui trazemos algumas informações sobre declaração de variaveis

---
+++ Nomes significativos/informacoes.java
@@ -0,0 +1,17 @@
+/*Passae dicas falsas ou que confundam quem vai ler seu codigo, nomes como hr, aix, sco seriam
+nomes ruins para variaveis assim como accountList o ideal seria accountGroup ou bunchOfAcconts*/
+
+int xyzControllerForEfficientHandlingOfStrings;
+int xyzControllerForEfficientStorageOfStrings;
+
+//nomes muito semelhantes e longos não são nada eficientes e se tornam confusos
+/*Tambem devesmos tomar cuidado com algumas letras como o "l" minusculo e o "o" 
+maiusculo por se parcer muito com 1 e 0*/
+
+int a = l;
+if(O == l)
+    a = O1;
+else
+    l = 01;
+
+//confuso e desnecessário
